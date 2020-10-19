6446d04409012644cd04f780ee8c0ea62ba70d0a
jmacd <jmacd@lightstep.com>
2020-10-19 00:21:12 -0700

    Finish SD->LS

--- RELEASE-PROCESS.md
+++ RELEASE-PROCESS.md
@@ -1,7 +1,7 @@
 # Release Process
 
-TODO(jmacd):
-Make sure the [end-to-end test](https://github.com/Stackdriver/stackdriver-prometheus-e2e) passes:
+TODO(jmacd): The following repository does not exist, and this test does not run; run this test in CI.
+Make sure the [end-to-end test](https://github.com/lightstep/lightstep-prometheus-e2e) passes:
 ```sh
 export KUBE_CLUSTER=integration-cluster
 # Random namespace name in the form e2e-xxxxxxxx.
@@ -11,7 +11,7 @@ export GCP_REGION=us-central1-a
 
 make DOCKER_IMAGE_TAG=$USER push
 ( cd kube/full ; SIDECAR_IMAGE_TAG=$USER ./deploy.sh )
-( cd ../stackdriver-prometheus-e2e ; make )
+( cd ../lightstep-prometheus-e2e ; make )
 kubectl delete namespace ${KUBE_NAMESPACE}
 ```
 
--- kube/patch-operated.sh
+++ kube/patch-operated.sh
@@ -12,13 +12,14 @@ kubectl -n "${KUBE_NAMESPACE}" patch prometheus "$1" --type merge --patch "
 spec:
   containers:
   - name: sidecar
-    image: gcr.io/stackdriver-prometheus/stackdriver-prometheus-sidecar:${SIDECAR_IMAGE_TAG}
+    image: ${SIDECAR_IMAGE_NAME}:${SIDECAR_IMAGE_TAG}
     imagePullPolicy: Always
     args:
-    - \"--stackdriver.project-id=${GCP_PROJECT}\"
     - \"--prometheus.wal-directory=/data/wal\"
-    - \"--stackdriver.kubernetes.location=${GCP_REGION}\"
-    - \"--stackdriver.kubernetes.cluster-name=${KUBE_CLUSTER}\"
+    - \"--opentelemetry.api-address=https:${OTLP_DESTINATION}\"
+    - \"--grpc.header=Lightstep-Access-Token=${TOKEN}\"
+    - \"--resource.attribute=region=${GCP_REGION}\"
+    - \"--resource.attribute=cluster=${KUBE_CLUSTER}\"
     ports:
     - name: sidecar
       containerPort: 9091
