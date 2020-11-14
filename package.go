5c4153c75b891729e9ca99abde695cc79e7731d8
Josh MacDonald <jmacd@lightstep.com>
2020-11-14 01:18:44 -0800

    Use two instrumentation library names

--- package.go
+++ package.go
@@ -1,5 +1,24 @@
 package sidecar
 
+import (
+	"github.com/prometheus/common/version"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/global"
+)
+
 const (
-	InstrumentationLibrary = "github.com/lightstep/opentelemetry-prometheus-sidecar"
+	// Data exported from Prometheus are recored as:
+	ExportInstrumentationLibrary = "prometheus-sidecar"
+
+	// Diagnostics about this process are recorded as:
+	SelfInstrumentationLibrary = "github.com/lightstep/opentelemetry-prometheus-sidecar"
+)
+
+var (
+	OTelMeter = global.Meter(
+		SelfInstrumentationLibrary,
+		otel.WithInstrumentationVersion(version.Version),
+	)
+
+	OTelMeterMust = otel.Must(OTelMeter)
 )
