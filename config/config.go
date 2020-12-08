a7e1d1d1cde025c21e7dd36476bd91970d49c987
Josh MacDonald <jmacd@lightstep.com>
2020-12-08 13:48:14 -0800

    More docstring

--- config/config.go
+++ config/config.go
@@ -204,7 +204,7 @@ func Configure(args []string, readFunc FileReadFunc) (MainConfig, map[string]str
 
 	a.Flag(promlogflag.LevelFlagName, promlogflag.LevelFlagHelp).StringVar(&cfg.LogConfig.Level)
 	a.Flag(promlogflag.FormatFlagName, promlogflag.FormatFlagHelp).StringVar(&cfg.LogConfig.Format)
-	a.Flag("log.verbose", "Verbose level: 0 = off, 1 = some, 2 = more; add 1 if log.level is 'debug'").
+	a.Flag("log.verbose", "Verbose logging level: 0 = off, 1 = some, 2 = more; 1 is automatically added when log.level is 'debug'; impacts logging from the gRPC library in particular").
 		IntVar(&cfg.LogConfig.Verbose)
 
 	_, err := a.Parse(args[1:])
