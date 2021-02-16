50e35acf3245a93ff4b25d41dd319135eb9c3d61
Joshua MacDonald <jmacd@lightstep.com>
2021-02-16 09:01:28 -0800

    Braindump

--- tail/tail.go
+++ tail/tail.go
@@ -19,6 +19,7 @@ import (
 	"context"
 	"io"
 	"io/ioutil"
+	"net/url"
 	"os"
 	"path/filepath"
 	"sort"
@@ -30,6 +31,7 @@ import (
 	"github.com/go-kit/kit/log/level"
 	sidecar "github.com/lightstep/opentelemetry-prometheus-sidecar"
 	"github.com/lightstep/opentelemetry-prometheus-sidecar/config"
+	"github.com/lightstep/opentelemetry-prometheus-sidecar/prometheus"
 	"github.com/lightstep/opentelemetry-prometheus-sidecar/telemetry/doevery"
 	"github.com/pkg/errors"
 	"github.com/prometheus/prometheus/tsdb/record"
@@ -64,7 +66,8 @@ type Tailer struct {
 	dir string
 	cur io.ReadCloser
 
-	logger log.Logger
+	logger  log.Logger
+	promURL *url.URL
 
 	mtx         sync.Mutex
 	nextSegment int
@@ -75,11 +78,12 @@ type Tailer struct {
 // are read before reading any WAL segments.
 // Tailing may fail if we are racing with the DB itself in deleting obsolete checkpoints
 // and segments. The caller should implement relevant logic to retry in those cases.
-func Tail(ctx context.Context, logger log.Logger, dir string) (*Tailer, error) {
+func Tail(ctx context.Context, logger log.Logger, dir string, promURL *url.URL) (*Tailer, error) {
 	t := &Tailer{
-		ctx:    ctx,
-		dir:    dir,
-		logger: logger,
+		ctx:     ctx,
+		dir:     dir,
+		logger:  logger,
+		promURL: promURL,
 	}
 	cpdir, k, err := wal.LastCheckpoint(dir)
 	if errors.Cause(err) == record.ErrNotFound {
@@ -235,6 +239,15 @@ func (t *Tailer) Read(b []byte) (int, error) {
 				)
 			})
 
+			// Note: This condition here may leave us deadlocked if the
+			// Prometheus server decided to clean old WAL segments while
+			// we are reading them.  Note: a similar mechanism could be
+			// used here as the one described below.  If we stall at
+			// this point, and instead of waiting for a fixed time or
+			// the context to be canceled, wait until we see another WAL
+			// segment growing again.  If there's a gap, we should probably
+			// reset the stream.
+
 			select {
 			case <-time.After(backoff):
 			case <-t.ctx.Done():
@@ -249,47 +262,123 @@ func (t *Tailer) Read(b []byte) (int, error) {
 			return n, errors.Wrap(err, "open next segment")
 		}
 
-		offset := t.currentOffset()
+		// Note: Prometheus DOES NOT fsync the old segment
+		// before opening the new one.  We have a guessing game.
+		currentOffset := t.currentOffset()
 
 		for {
-			if offset == promSegmentSize {
-				level.Info(t.logger).Log(
+			// If we finished a WAL segment, good!
+			if currentOffset == promSegmentSize {
+				level.Debug(t.logger).Log(
 					"msg", "WAL segment complete",
 					"segment", segment-1,
 				)
 				break
 			}
 
-			// @@@ HERE:
-			//   prometheus.WaitForReady()
-			//   does Prom give you info about PID or something we could use in /metrics?
-			// Try to read more from the same file.
-			//   look for at least a full block.
-			//   Prometheus typically zero-pads its final block.
-			//   yet we see this fail.
-			// When we still have a partial block after all of this, we need a
-			// way to force the reader to re-open.
+			// Check if Prometheus is still ready, as a
+			// stall and to help diagnose problems in this area.
+			// Note: The sidecar in general has no mechanism to
+			// gracefully deal with Prometheus crashing while the
+			// sidecar is healthy.  TODO: consider this situation.
+			err := func() error {
+				ctx, cancel := context.WithTimeout(
+					context.Background(),
+					config.DefaultHealthCheckTimeout)
+				defer cancel()
+				return prometheus.WaitForReady(ctx, t.logger, t.promURL)
+			}()
 
-			// Having discovered a new segment, give the the current segment
-			// a second try.
+			if err != nil {
+				// Note: this blocks until either the context is
+				// canceled (e.g., by the SIGTERM handler) or Prometheus
+				// succeeds.  This indicates the context was canceled.
+				return 0, errors.Wrap(err, "waiting for WAL")
+			}
+
+			// There's no harm in trying again. We have no idea yet
+			// whether there's an fsync() to wait for, but if this
+			// Read() succeeds again we're happy.
 			if n, err = t.cur.Read(b); err != io.EOF {
 				t.incOffset(n)
 				return n, err
+			}
+
+			// OK, what now?  There are several known possibilities.  A
+			// healthy Prometheus appears to always leave files that are
+			// a multiple of the block size.  If the file ends at a block
+			// size multiple, we should not expect corruption and the next
+			// Read() should begin at offset 0 in the folling WAL segment.
+			//
+			// However, more auditing of the Prometheus code would be needed
+			// to be convinced it's not possible for more blocks to appear
+			// in the file at this point.
+			//
+			// It seems to be worth waiting a while, as a (poor) safety
+			// mechanism.  30 seconds?  a minute?  What we want to know
+			// is whether Prometheus has begun writing to a new segment.
+			// Once we see it writing (or any change in the WAL
+			// directory) and wait sufficiently long for fsync() to
+			// complete, we may be fairly sure that the current segment
+			// is finished.
+			//
+			// TODO: Wait up to 60 seconds or so, or until we see progress
+			// on another WAL segment?  This means calling ReadDir() and
+			// checking for changes, I guess.
+			if weAreSurelyReadyToProceed() {
+				break
 			}
 		}
 
 		finalOffset := t.incNextSegment()
 
-		if finalOffset&(promPageSize-1) != 0 {
-			// Note: Prometheus DOES NOT fsync the old segment
-			// before opening the new one.
-
-			return n, errors.Errorf(
-				"segment %d transition at unexpected offset: %d",
-				segment-1,
-				finalOffset,
-			)
-		}
+		// Note: This code has triggered in production in a situation where
+		// Prometheus did not detect corruption.
+		//
+		// if finalOffset&(promPageSize-1) != 0 {
+		// 	return n, errors.Errorf(
+		// 		"segment %d transition at unexpected offset: %d",
+		// 		segment-1,
+		// 		finalOffset,
+		// 	)
+		// }
+		//
+		// We are seeing these "unexpected" conditions but aren't quite sure
+		// why.  Either the Prom has crashed with an incomplete WAL block,
+		// or Prom is still busy writing that WAL block.  Either way, it's
+		// not safe to transition from one WAL segment to another when not
+		// at a block size multiple, but this requires understanding the
+		// Prometheus logic.
+		//
+		// In prometheus/tsdb/wal/wal.go, find the function `NewSegmentBufReader()`.
+		// This is the code that Prometheus uses to read the WAL at startup
+		// and it is coded to deal specifically with the problem we're facing.
+		// See their comment:
+		//
+		//     We hit EOF; fake out zero padding at the end of short segments, so we
+		//     don't increment curr too early and report the wrong segment as corrupt.
+		//
+		// This code appears to allow Prometheus to recover to health even when
+		// there are incomplete blocks.  It does not necesarily imply corruption,
+		// as seen in the transcript of
+		// https://github.com/lightstep/opentelemetry-prometheus-sidecar/issues/108
+		// where Prometheus recovered into a healthy state while sidecar hit
+		// corruption.
+		//
+		// To address this, I see two ways forward:
+		// 1. This code can insert zero padding when it detects a non-block-aligned
+		//    WAL segment transition.
+		// 2. This code can return failures that the reader recovers from, essentially
+		//    force it to re-open its reader when something bad happens.
+		//
+		// The second approach will work in a more general sense, since
+		// there are other conditions that may cause us to require the
+		// reader to reset.  Ideally, we do not crash in this situation, because
+		// we have a warm metadata cache.
+		//
+		// To be concrete, suppose that Read returns a special kind of error
+		// to indicate suspected corruption.  This will propagate up to
+		// (*PrometheusReader).Run() which will have to start again.
 
 		t.cur = next
 	}
