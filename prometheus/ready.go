4b703bf9888584521784bd75d33dd290806378c2
Joshua MacDonald <jmacd@lightstep.com>
2021-02-18 08:53:35 -0800

    Fix FD leak

--- prometheus/ready.go
+++ prometheus/ready.go
@@ -30,18 +30,26 @@ func WaitForReady(ctx context.Context, logger log.Logger, promURL *url.URL) erro
 			return errors.Wrap(err, "build request")
 		}
 
-		resp, err := http.DefaultClient.Do(req)
-		if err == nil && resp.StatusCode/100 == 2 {
-			return nil
-		}
+		success := func() bool {
+			resp, err := http.DefaultClient.Do(req)
+			defer resp.Body.Close()
+			if err == nil && resp.StatusCode/100 == 2 {
+				return true
+			}
 
-		if warnCount != 0 {
+			if warnCount == 0 {
+				return false
+			}
 			if err != nil {
 				level.Warn(logger).Log("msg", "Prometheus readiness check", "err", err)
 			} else {
 				level.Warn(logger).Log("msg", "Prometheus is not ready", "status", resp.Status)
 			}
 			warnCount++
+			return false
+		}()
+		if success {
+			return nil
 		}
 
 		select {
