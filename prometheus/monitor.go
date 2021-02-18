b0e5dfb8e046523a6aa2d171c8ccd5f56f856021
Joshua MacDonald <jmacd@lightstep.com>
2021-02-17 21:02:51 -0800

    Test simple Prometheus scraper

--- prometheus/monitor.go
+++ prometheus/monitor.go
@@ -8,6 +8,7 @@ import (
 	"github.com/lightstep/opentelemetry-prometheus-sidecar/telemetry"
 	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/prom2json"
+	"github.com/prometheus/prometheus/pkg/labels"
 )
 
 var monitorDuration = telemetry.NewTimer(
@@ -20,7 +21,13 @@ type (
 		target *url.URL
 	}
 
-	Result map[string]*dto.MetricFamily
+	Family struct {
+		family *dto.MetricFamily
+	}
+
+	Result struct {
+		values map[string]Family
+	}
 )
 
 func NewMonitor(target *url.URL) *Monitor {
@@ -32,8 +39,10 @@ func NewMonitor(target *url.URL) *Monitor {
 func (m *Monitor) Get() (_ Result, retErr error) {
 	var (
 		wg  sync.WaitGroup
-		res = Result{}
 		ch  = make(chan *dto.MetricFamily)
+		res = Result{
+			values: map[string]Family{},
+		}
 	)
 
 	defer monitorDuration.Start(context.Background()).Stop(&retErr)
@@ -44,12 +53,67 @@ func (m *Monitor) Get() (_ Result, retErr error) {
 	go func() {
 		defer wg.Done()
 		for mfam := range ch {
-			if mfam.Name != nil {
-				res[*mfam.Name] = mfam
+			res.values[mfam.GetName()] = Family{
+				family: mfam,
 			}
 		}
 	}()
 
 	// Note: FetchMetricFamilies closes the channel.
 	return res, prom2json.FetchMetricFamilies(m.target.String(), ch, nil)
+}
+
+func (r Result) Counter(name string) Family {
+	f := r.values[name]
+	if f.family.GetType() != dto.MetricType_COUNTER {
+		return Family{}
+	}
+	return f
+}
+
+func (r Result) Gauge(name string) Family {
+	f := r.values[name]
+	if f.family.GetType() != dto.MetricType_GAUGE {
+		return Family{}
+	}
+	return f
+}
+
+func exactMatch(query map[string]string, ls []*dto.LabelPair) bool {
+	if len(ls) != len(query) {
+		return false
+	}
+	for _, l := range ls {
+		if l == nil || l.Name == nil || l.Value == nil {
+			return false
+		}
+		if query[*l.Name] != *l.Value {
+			return false
+		}
+	}
+	return true
+}
+
+func (f Family) For(ls labels.Labels) float64 {
+	if f.family == nil {
+		return 0
+	}
+	match := ls.Map()
+	for _, m := range f.family.Metric {
+		if !exactMatch(match, m.Label) {
+			continue
+		}
+
+		switch f.family.GetType() {
+		case dto.MetricType_COUNTER:
+			if m.Counter != nil && m.Counter.Value != nil {
+				return *m.Counter.Value
+			}
+		case dto.MetricType_GAUGE:
+			if m.Gauge != nil && m.Gauge.Value != nil {
+				return *m.Gauge.Value
+			}
+		}
+	}
+	return 0
 }
--- prometheus/monitor_test.go
+++ prometheus/monitor_test.go
@@ -1,27 +1,56 @@
 package prometheus
 
 import (
-	"context"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
+
+	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMonitorScrape(t *testing.T) {
-
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := json.NewEncoder(w).Encode(
-		// handler(
-		// r.FormValue("metric"),
-		// r.FormValue("match_target"),
-		// )
-		)
-		if err != nil {
-			t.Fatal(err)
-		}
+		_, err := w.Write([]byte(`
+# HELP http_requests_total The total number of HTTP requests.
+# TYPE http_requests_total counter
+http_requests_total{method="post",code="200"} 1027 1395066363000
+http_requests_total{method="post",code="400"}    3 1395066363000
+
+# HELP blah blah
+# TYPE utilization gauge
+utilization{} 123
+`))
+		require.NoError(t, err)
 	}))
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+
+	tu, err := url.Parse(ts.URL)
+	require.NoError(t, err)
+
+	m := NewMonitor(tu)
+
+	res, err := m.Get()
+	require.NoError(t, err)
+
+	// Positive examples
+	require.Equal(t, 1027.0, res.Counter("http_requests_total").
+		For(labels.FromStrings("method", "post", "code", "200")))
+
+	require.Equal(t, 3.0, res.Counter("http_requests_total").
+		For(labels.FromStrings("method", "post", "code", "400")))
+
+	require.Equal(t, 123.0, res.Gauge("utilization").
+		For(labels.FromStrings()))
+
+	// Negative examples
+	require.Equal(t, 0.0, res.Counter("http_requests_total").
+		For(labels.FromStrings("method", "post", "code", "400", "user", "nobody")))
+
+	require.Equal(t, 0.0, res.Counter("http_requests_total").
+		For(labels.FromStrings("method", "post", "code", "500")))
+
+	require.Equal(t, 0.0, res.Counter("other_requests_total").
+		For(labels.FromStrings("method", "post", "code", "400")))
 
 }
