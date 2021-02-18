96b8d0374d7e7adbf1dcd2fac50208c4f609068c
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2021-02-18 18:29:41 +0100

    Remove the use_meta_labels parameter.
    Now that we are not fetching targets/, this is
    useless.

--- config/config.go
+++ config/config.go
@@ -152,7 +152,6 @@ type PromConfig struct {
 
 type OTelConfig struct {
 	MetricsPrefix string `json:"metrics_prefix"`
-	UseMetaLabels bool   `json:"use_meta_labels"`
 }
 
 type AdminConfig struct {
@@ -283,9 +282,6 @@ func Configure(args []string, readFunc FileReadFunc) (MainConfig, map[string]str
 
 	a.Flag("opentelemetry.metrics-prefix", "Customized prefix for exporter metrics. If not set, none will be used").
 		StringVar(&cfg.OpenTelemetry.MetricsPrefix)
-
-	a.Flag("opentelemetry.use-meta-labels", "Prometheus target labels prefixed with __meta_ map into labels.").
-		BoolVar(&cfg.OpenTelemetry.UseMetaLabels)
 
 	a.Flag("filter", "PromQL metric and label matcher which must pass for a series to be forwarded to OpenTelemetry. If repeated, the series must pass any of the filter sets to be forwarded.").
 		StringsVar(&cfg.Filters)
